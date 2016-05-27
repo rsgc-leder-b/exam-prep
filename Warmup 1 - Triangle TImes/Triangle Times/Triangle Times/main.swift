088111d07f17151c08dee9ca2bb8a119a7f0f019
rsgc-leder-b <bleder@rsgc.on.ca>
2016-05-27 10:04:47 -0400

    Warmup 1 Complete

---
+++ Warmup 1 - Triangle TImes/Triangle Times/Triangle Times/main.swift
@@ -0,0 +1,56 @@
+//
+//  main.swift
+//  Triangle Times
+//
+//  Created by Brendan Leder on 2016-05-27.
+//  Copyright © 2016 Brendan Leder. All rights reserved.
+//
+
+var properInput = false
+var angle1 = 0
+var angle2 = 0
+var angle3 = 0
+
+while (!properInput) {
+    print("Angle 1? ", terminator: "")
+    if let angleString = readLine(stripNewline: true) {
+        if let angle = Int(angleString) {
+            angle1 = angle
+            properInput = true
+        }
+    }
+}
+
+properInput = false
+
+while (!properInput) {
+    print("Angle 2? ", terminator: "")
+    if let angleString = readLine(stripNewline: true) {
+        if let angle = Int(angleString) {
+            properInput = true
+            angle2 = angle
+        }
+    }
+}
+
+properInput = false
+
+while (!properInput) {
+    print("Angle 3? ", terminator: "")
+    if let angleString = readLine(stripNewline: true) {
+        if let angle = Int(angleString) {
+            properInput = true
+            angle3 = angle
+        }
+    }
+}
+
+if (angle1 + angle2 + angle3 != 180) {
+    print("Error")
+} else if (angle1 == angle2 && angle2 == angle3) {
+    print("Equilateral")
+} else if (angle1 == angle2 || angle2 == angle3 || angle3 == angle1) {
+    print("Isosceles")
+} else {
+    print("Scalene")
+}
